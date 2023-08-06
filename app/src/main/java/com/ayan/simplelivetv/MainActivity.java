c8fba4221b54269db6ebfaf83747d841027f6a07
Ayan-Roy <@Aroy0115>
2023-08-06 22:08:47 +0600

    Internet Permission

--- app/src/main/java/com/ayan/simplelivetv/MainActivity.java
+++ app/src/main/java/com/ayan/simplelivetv/MainActivity.java
@@ -3,12 +3,27 @@ package com.ayan.simplelivetv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    String[] str_permission = {"android.permission.INTERNET"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Hooks
+        videoView = findViewById(R.id.video_view);
+
+
+        CheckInternetPermission();
+    }
+
+    private void CheckInternetPermission() {
+        requestPermissions(str_permission, 100);
     }
 }
